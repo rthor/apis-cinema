88af72e08204be01408d7b7cba1e928e67bbd21d
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2014-01-14 20:22:22 +0000

    Add Rotten Tomateo link and rating

--- root.js
+++ root.js
@@ -1,6 +1,6 @@
 module.exports = function(options, callback) {
   var results = [];
-  var url = 'http://kvikmyndir.is/api/showtimes/?key=T6hxxd2Ks701dqjpbgKjkGA94QE7znKl';
+  var url = 'http://kvikmyndir.is/api/showtimes/?key=';
   
   require('request').get(url, function parseData (err, movies, body) {
     JSON.parse( body ).forEach(function (movie, i) {
@@ -10,6 +10,8 @@ module.exports = function(options, callback) {
         restricted: movie.certificateIS,
         imdb: movie.ratings.imdb + '/10',
         imdbLink: 'http://www.imdb.com/title/tt' + movie.ids.imdb,
+        rotten: movie.ratings.rotten_critics ? '' + parseInt(movie.ratings.rotten_critics, 10) / 10 : '',
+        rottenLink: movie.ids.rotten ? 'http://www.rottentomatoes.com/m/' + movie.ids.rotten : '',
         image: movie.poster,
         showtimes: movie.showtimes.map( require('./helpers/fix-showtime') )
       });
