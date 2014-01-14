728dc645bbb58b8836e68c4e01f9f1efa86fdab2
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2014-01-14 19:48:59 +0000

    Simplify everything!

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-var request = require('request')
-  , fixShowtime = require('./helpers/fix-showtime')
-  , results = []
-  , imdbBase = 'http://www.imdb.com/title/tt';
-
 exports.is = function(options, callback) {
-  request.get({
-    url: 'http://kvikmyndir.is/api/showtimes/?key='
-  }, function parseData (err, movies, body) {
-
-    movies = JSON.parse( body );
-
-    movies.forEach(function (movie, i){
+  var results = [];
+  var url = 'http://kvikmyndir.is/api/showtimes/?key=';
+  
+  require('request').get(url, function parseData (err, movies, body) {
+    JSON.parse( body ).forEach(function (movie, i) {
       results.push({
         title: movie.title,
         released: movie.year,
         restricted: movie.certificateIS,
         imdb: movie.ratings.imdb + '/10',
-        imdbLink: imdbBase + movie.ids.imdb,
+        imdbLink: 'http://www.imdb.com/title/tt' + movie.ids.imdb,
         image: movie.poster,
-        showtimes: movie.showtimes.map( fixShowtime )
+        showtimes: movie.showtimes.map( require('./helpers/fix-showtime') )
       });
     });
 
-    return callback(null, { results: results });
+    callback(null, { results: results });
   });
 };
