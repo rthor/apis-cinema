114a1841a00dcc295a68d317346af8726512c345
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2014-01-14 19:54:10 +0000

    Rootify

---
+++ root.js
@@ -0,0 +1,20 @@
+module.exports = function(options, callback) {
+  var results = [];
+  var url = 'http://kvikmyndir.is/api/showtimes/?key=T6hxxd2Ks701dqjpbgKjkGA94QE7znKl';
+  
+  require('request').get(url, function parseData (err, movies, body) {
+    JSON.parse( body ).forEach(function (movie, i) {
+      results.push({
+        title: movie.title,
+        released: movie.year,
+        restricted: movie.certificateIS,
+        imdb: movie.ratings.imdb + '/10',
+        imdbLink: 'http://www.imdb.com/title/tt' + movie.ids.imdb,
+        image: movie.poster,
+        showtimes: movie.showtimes.map( require('./helpers/fix-showtime') )
+      });
+    });
+
+    callback(null, { results: results });
+  });
+};
