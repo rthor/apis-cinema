d0724ae1997cc9c01e906b74293cc35ac2663b2c
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2014-03-11 14:21:51 +0000

    Abstract callbacks

--- theaters.js
+++ theaters.js
@@ -2,31 +2,32 @@ var _ = require('lodash');
 var getMovies = require('./helpers/get-movies');
 var theaters = require('./helpers/theaters.js');
 
-function mapSchedule (time) {
-  return time.trim();
+function iterateTheaterMovies (movie) {
+  var movieObj = _.where(movie.showtimes, {cinema: '' + this.id});
+
+  if (movieObj.length) {
+    this.movies.push({
+      title: movie.title,
+      schedule: _.map(
+        movieObj[0].schedule,
+        function (time) { return time.trim(); }
+      )
+    });
+  }
 }
 
-module.exports = function getTheaters (options, callback) {
-  var movieObj = {};
+function mapTheaters (theater) {
+  theater.movies = [];
+  this.forEach( iterateTheaterMovies.bind(theater) );
+  return theater;
+}
 
-  return getMovies(function parseMovies (movies) {
-    return callback(null, {
-      results: _.map(theaters, function mapTheaters (theater) {
-        theater.movies = [];
-
-        movies.forEach(function iterateMovies (movie) {
-          movieObj = _.where(movie.showtimes, {cinema: '' + theater.id});
-
-          if (movieObj.length) {
-            theater.movies.push({
-              title: movie.title,
-              schedule: _.map(movieObj[0].schedule, mapSchedule)
-            });
-          }
-        });
-
-        return theater;
-      })
-    });
+function parseMovies (movies) {
+  return this(null, {
+    results: _.map(theaters, mapTheaters.bind(movies))
   });
+}
+
+module.exports = function getTheaters (config, cb) {
+  return getMovies( parseMovies.bind(cb) );
 };
