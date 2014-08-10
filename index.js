0a9111239cc09ebd6d0a4c2df2c6aef92e934833
Ragnar Þór Valgeirsson <ragnar.valgeirsson@gmail.com>
2014-08-10 15:56:48 +0000

    Abstract endpoint from kristjanmik/apis

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = function(app, prefix) {};
+var parser = require('apis-parser');
+var endpoints = require('fs').readdirSync(__dirname + '/lib/');
+
+module.exports = function(app, prefix) {
+  endpoints.forEach(function(endpoint) {
+    if (/\.js$/ig.test(endpoint)) {
+      prefix += endpoint === 'cinema.js' ? '' : ('/' + endpoint.replace('.js', ''));
+      app.get(prefix, function(req, res) {
+        parser(res)(function(callback) {
+          require('./lib/' + endpoint)(callback);
+        });
+      });
+    }
+  });
+};
